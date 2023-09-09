cb258d9e8823cb9d9fbb342cf2173523e13c92f3
The Xsoft Tech <127049334+XsoftBud@users.noreply.github.com>
2023-09-09 06:58:26 -0700

    Add files via upload

---
+++ xpt.py
@@ -0,0 +1,1 @@
+import turtlea=turtle.Turtle()a.color('green1')a.speed(0)a.hideturtles=turtle.Screen()s.bgcolor('black')for x in range(200):    a.forward(x)    a.left(x-1)turtle.done()
---
+++ xv.bat
@@ -0,0 +1,3 @@
+@echo off
+python xtp.py
+exit
